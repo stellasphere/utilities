63be3e86ae3b4d1dfc108ea8a6ad5492016cd63c
Stellasphere <leoleoueno@gmail.com>
2021-10-09 23:24:47 +0000

    Added a Node.js version.

---
+++ nodejs.js
@@ -0,0 +1,11 @@
+module.exports = {
+  keyValueObjectToArrays: function(keyValueObject) {
+    var keys = Object.keys(keyValueObject);
+    var values = [];
+    for(i in keys) {
+      var key = keys[i];
+      values.push(keyValueObject[key]);
+    }
+    return [keys,values]
+  }
+}
