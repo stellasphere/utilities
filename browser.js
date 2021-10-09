7e4bdd12fff938333fde11eb02ac866d485d7b7c
Stellasphere <leoleoueno@gmail.com>
2021-10-09 23:21:49 +0000

    Removed the test defaults.

--- browser.js
+++ browser.js
@@ -1,6 +1,5 @@
 var utilities = {
   keyValueObjectToArrays: function(keyValueObject) {
-    var keyValueObject = {train: 76, valid: 28, test: 19}
     var keys = Object.keys(keyValueObject);
     var values = [];
     for(i in keys) {
