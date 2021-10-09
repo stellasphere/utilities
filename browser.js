4a9ba7f2b2ff354fc70e5d0c541c82b80d7e427b
Stellasphere <leoleoueno@gmail.com>
2021-10-09 23:59:10 +0000

    Added a keyValueObjectToArrayObjects function.

--- browser.js
+++ browser.js
@@ -7,5 +7,17 @@ var utilities = {
       values.push(keyValueObject[key]);
     }
     return [keys,values]
+  },
+  keyValueObjectToArrayObjects: function(keyValueObject) {
+    var keys = Object.keys(keyValueObject);
+    var objects = [];
+    for(i in keys) {
+      var key = keys[i];
+      var keyValue = {};
+      keyValue.key = key;
+      keyValue.value = keyValueObject[key];
+      objects.push(keyValue);
+    }
+    return objects
   }
 }
--- nodejs.js
+++ nodejs.js
@@ -7,5 +7,17 @@ module.exports = {
       values.push(keyValueObject[key]);
     }
     return [keys,values]
+  },
+  keyValueObjectToArrayObjects: function(keyValueObject) {
+    var keys = Object.keys(keyValueObject);
+    var objects = [];
+    for(i in keys) {
+      var key = keys[i];
+      var keyValue = {};
+      keyValue.key = key;
+      keyValue.value = keyValueObject[key];
+      objects.push(keyValue);
+    }
+    return objects
   }
 }
