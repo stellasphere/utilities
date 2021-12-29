b1990b11a167207085610eef21404e1def312b77
Stellasphere <leoleoueno@gmail.com>
2021-12-29 21:44:21 +0000

    Fixed hour unit of the millisecondsToRelativeTime function.

--- nodejs.js
+++ nodejs.js
@@ -55,8 +55,8 @@ module.exports = {
     return [...new Set(array)];
   },
   millisecondsToRelativeTime: function(ms) {
-    var hours = Math.floor(ms/3600000);
-    ms -= hours*3600000;
+    var hours = Math.floor(ms/360000);
+    ms -= hours*360000;
     var minutes = Math.floor(ms/60000);
     ms -= minutes*60000;
     var seconds = Math.floor(ms/1000);
